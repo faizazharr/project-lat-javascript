948442811dcf9c23cb9f6f6d0faec4e10ab97788
faiz azhar ristya nugraha <faizazharr@gmail.com>
2019-01-27 15:04:26 +0700

    add page admin

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-// setup home index
+// setup route home index
 router.get('/', function(req, res) {
     res.json('Ini Admin Area');
 });
 
+//setup route add-page
+router.get('/add-page', function(req, res) {
+    var title = "";
+    var slug = "";
+    var content = "";
+
+    res.render('admin/add_page', {
+        title: title,
+        slug: slug,
+        content: content
+    })
+});
 
 module.exports = router;
