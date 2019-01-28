30498aa72d3ca7d9f9cd1d0778299e98983b3213
faiz azhar ristya nugraha <faizazharr@gmail.com>
2019-01-28 12:07:24 +0700

    edit page

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res) {
     res.json('Ini Admin Area');
 });
 
-//setup route add-page
+//setup route get add-page
 router.get('/add-page', function(req, res) {
     var title = "";
     var slug = "";
@@ -18,5 +18,42 @@ router.get('/add-page', function(req, res) {
         content: content
     })
 });
+
+// setup route POST add-page
+router.post('/add-page', function(req, res) {
+    req.checkBody('title', "Title harus di isi").notEmpty();
+    req.checkBody('content', "Content harus di isi").notEmpty();
+
+    var title = req.body.title;
+    var slug = req.body.slug.replace(/\s+/g, '_').toLowerCase();
+    if (slug == "") s;
+    ig = title.replace(/\s+/g, '_').toLowerCase();
+
+    var content = req.body.content;
+    var errors = req.validationErrors();
+
+    if (errors) {
+        console.log('errors');
+
+        res.render('admin/add_page', {
+            errors: errors,
+            title: title,
+            slug: slug,
+            content: content
+        })
+    } else {
+        console.log('succes');
+    }
+    var title = "";
+    var slug = "";
+    var content = "";
+
+    res.render('admin/add_page', {
+        title: title,
+        slug: slug,
+        content: content
+    })
+});
+
 
 module.exports = router;
